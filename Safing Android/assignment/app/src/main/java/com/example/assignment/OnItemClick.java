b001d3bd4928d7de142925b070a8a8f94b269d08
명운 <48548596+WAWWW121@users.noreply.github.com>
2022-01-21 12:45:02 +0900

    다시 test

--- Safing Android/assignment/app/src/main/java/com/example/assignment/OnItemClick.java
+++
@@ -1,5 +0,0 @@
-package com.example.assignment;
-
-public interface OnItemClick {
-    void onClick (String value);
-}
--- Safing Android/assignment/app/src/main/java/com/example/assignment/async/CommonVal.java
+++
@@ -1,7 +0,0 @@
-package com.example.assignment.async;
-
-import com.example.newlastproject.member.MemberVO;
-
-public class CommonVal {
-    public static MemberVO loginInfo;
-}
