63d6e0f6b88519581d0ebbccc3b464985bd5eadb
명운 <48548596+WAWWW121@users.noreply.github.com>
2022-01-28 11:55:19 +0900

    잘못올린거

---
+++ Safing Android/Safing/app/src/main/java/com/example/safing/activity/Purchase_Result_Activity.java
@@ -0,0 +1,69 @@
+package com.example.safing.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+<<<<<<< HEAD
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+=======
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+>>>>>>> origin/기근태
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.safing.R;
+<<<<<<< HEAD
+import com.example.safing.fragment.Product_PurchaseHistory_Fragment;
+=======
+>>>>>>> origin/기근태
+
+public class Purchase_Result_Activity extends AppCompatActivity {
+    TextView purchase_result_tv1;
+    Button purchase_result_btn1, purchase_result_btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+<<<<<<< HEAD
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+=======
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+
+>>>>>>> origin/기근태
+        setContentView(R.layout.activity_purchase_result);
+
+        purchase_result_tv1 = findViewById(R.id.purchase_result_tv1);
+        purchase_result_btn1 = findViewById(R.id.purchase_result_btn1);
+        purchase_result_btn2 = findViewById(R.id.purchase_result_btn2);
+
+        purchase_result_btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Purchase_Result_Activity.this, MainActivity.class);
+                intent.putExtra("fragment", "Product_PurchaseHistory_Fragment");
+                startActivity(intent);
+            }
+        });
+
+        purchase_result_btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Purchase_Result_Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
