40dcbdc07ce1261ed23313254bd1a1f224c57d07
명운 <48548596+WAWWW121@users.noreply.github.com>
2022-01-28 14:45:46 +0900

    기본 버전 이걸로 작업 ㄱㄱ

--- Safing Android/Safing/app/src/main/java/com/example/safing/fragment/MypageFragment.java
+++ Safing Android/Safing/app/src/main/java/com/example/safing/fragment/MypageFragment.java
@@ -2,13 +2,6 @@ package com.example.safing.fragment;
 
 import android.content.Context;
 import android.content.Intent;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-import android.media.Image;
-=======
->>>>>>> origin/main
->>>>>>> parent of 74edf15 (01/27 20:38 최성욱)
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,8 +15,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.safing.R;
 import com.example.safing.activity.MainActivity;
-import com.example.safing.activity.QNAActivity;
-import com.example.safing.activity.SettingActivity;
 
 public class MypageFragment extends Fragment {
     Context context;
@@ -46,16 +37,8 @@ public class MypageFragment extends Fragment {
         // Inflate the layout for this fragment
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_mypage, container, false);
 
-<<<<<<< HEAD
         mainActivity = (MainActivity) getActivity();
 
-=======
-<<<<<<< HEAD
-=======
-        mainActivity = (MainActivity) getActivity();
-
->>>>>>> origin/main
->>>>>>> parent of 74edf15 (01/27 20:38 최성욱)
         mypage_setting = rootView.findViewById(R.id.mypage_setting);
         mypage_add = rootView.findViewById(R.id.mypage_add);
         mypage_login = rootView.findViewById(R.id.mypage_login);
@@ -67,15 +50,7 @@ public class MypageFragment extends Fragment {
             public void onClick(View v) {
                 mypage_login.setVisibility(View.GONE);
                 mypage_id.setVisibility(View.VISIBLE);
-<<<<<<< HEAD
                 mainActivity.changeFragment(new LoginFragment(context));
-=======
-<<<<<<< HEAD
-
-=======
-                mainActivity.changeFragment(new LoginFragment(context));
->>>>>>> origin/main
->>>>>>> parent of 74edf15 (01/27 20:38 최성욱)
             }
         });
 
@@ -84,32 +59,14 @@ public class MypageFragment extends Fragment {
         mypage_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-<<<<<<< HEAD
                 mainActivity.changeFragment(new QNAFragment(context));
-=======
-<<<<<<< HEAD
-                Intent intent = new Intent(context, QNAActivity.class);
-                startActivity(intent);
-=======
-                mainActivity.changeFragment(new QNAFragment(context));
->>>>>>> origin/main
->>>>>>> parent of 74edf15 (01/27 20:38 최성욱)
             }
         });
 
         mypage_setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-<<<<<<< HEAD
                 mainActivity.changeFragment(new SettingFragment(context));
-=======
-<<<<<<< HEAD
-                Intent intent = new Intent(context, SettingActivity.class);
-                startActivity(intent);
-=======
-                mainActivity.changeFragment(new SettingFragment(context));
->>>>>>> origin/main
->>>>>>> parent of 74edf15 (01/27 20:38 최성욱)
             }
         });
 
--- Safing Android/Safing/app/src/main/java/com/example/safing/fragment/Product_Purchase_Fragment.java
+++ Safing Android/Safing/app/src/main/java/com/example/safing/fragment/Product_Purchase_Fragment.java
@@ -2,18 +2,7 @@ package com.example.safing.fragment;
 
 import android.app.Activity;
 import android.content.Context;
-<<<<<<< HEAD
 import android.content.Intent;
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.Drawable;
-=======
->>>>>>> 기근태
->>>>>>> parent of 74edf15 (01/27 20:38 최성욱)
->>>>>>> d785786d5ea155274c65bfa3305b907797f3797b
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -21,80 +10,17 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
-<<<<<<< HEAD
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-=======
->>>>>>> 기근태
->>>>>>> parent of 74edf15 (01/27 20:38 최성욱)
->>>>>>> d785786d5ea155274c65bfa3305b907797f3797b
 
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-<<<<<<< HEAD
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.CheckBox;
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
->>>>>>> parent of 74edf15 (01/27 20:38 최성욱)
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-<<<<<<< HEAD
-import com.example.safing.R;
-import com.example.safing.activity.MainActivity;
-import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.tabs.TabLayout;
-
-
-public class Product_Purchase_Fragment extends Fragment {
-=======
-import com.example.safing.DTO.Product_Cart_RecDTO;
-import com.example.safing.DTO.Product_DetailDTO;
-import com.example.safing.R;
-import com.example.safing.activity.MainActivity;
-import com.example.safing.adapter.Producdt_Detail_Apdater;
-import com.example.safing.adapter.Product_Cart_Rec_Adapter;
-import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.tabs.TabLayout;
-
-import java.util.ArrayList;
-
-
-public class Product_Purchase_Fragment extends Fragment {
-    boolean isClick1 = false;
-    boolean isClick2 = false;
-    boolean isClick3 = false;
-    boolean isClick4 = false;
->>>>>>> parent of 74edf15 (01/27 20:38 최성욱)
-    Context context;
-    TabLayout product_purchase_tab1;
-    MainActivity mainActivity = new MainActivity();
-    Toolbar toolbar;
-    Animation rotate;
-    NavigationView product_purchase_view;
-<<<<<<< HEAD
-=======
-    RecyclerView product_purchase_rec1;
-    ImageView product_purchase_toggle1, product_purchase_toggle2, product_purchase_toggle3, product_purchase_toggle4;
-    LinearLayout product_purchase_updown1, product_purchase_updown2, product_purchase_updown3, product_purchase_updown4,product_purchase_updown5, product_purchase_updown6;
-=======
->>>>>>> d785786d5ea155274c65bfa3305b907797f3797b
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -125,16 +51,11 @@ public class Product_Purchase_Fragment extends Fragment {
     LinearLayoutManager manager;
     Toolbar toolbar;
     NavigationView product_purchase_view;
-<<<<<<< HEAD
     RecyclerView product_purchase_rec1;
     ImageView product_purchase_toggle1, product_purchase_toggle2, product_purchase_toggle3, product_purchase_toggle4;
     LinearLayout product_purchase_updown1, product_purchase_updown2, product_purchase_updown3, product_purchase_updown4,product_purchase_updown5, product_purchase_updown6;
     CheckBox product_purchase_box1, product_purchase_box2, product_purchase_box3, product_purchase_box4, product_purchase_box5, product_purchase_box6;
     Button product_purchase_btn1;
-=======
->>>>>>> 기근태
->>>>>>> parent of 74edf15 (01/27 20:38 최성욱)
->>>>>>> d785786d5ea155274c65bfa3305b907797f3797b
 
 
     public Product_Purchase_Fragment(Context context){
@@ -153,12 +74,6 @@ public class Product_Purchase_Fragment extends Fragment {
         product_purchase_tab1 = rootView.findViewById(R.id.product_purchase_tab1);
         toolbar = rootView.findViewById(R.id.product_purchase_toolbar);
         product_purchase_view = rootView.findViewById(R.id.product_purchase_view);
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> d785786d5ea155274c65bfa3305b907797f3797b
         product_purchase_rec1 = rootView.findViewById(R.id.product_purchase_rec1);
         product_purchase_toggle1 = rootView.findViewById(R.id.product_purchase_toggle1);
         product_purchase_toggle2 = rootView.findViewById(R.id.product_purchase_toggle2);
@@ -170,7 +85,6 @@ public class Product_Purchase_Fragment extends Fragment {
         product_purchase_updown4 = rootView.findViewById(R.id.product_purchase_updown4);
         product_purchase_updown5 = rootView.findViewById(R.id.product_purchase_updown5);
         product_purchase_updown6 = rootView.findViewById(R.id.product_purchase_updown6);
-<<<<<<< HEAD
         product_purchase_box1 = rootView.findViewById(R.id.product_purchase_box1);
         product_purchase_box2 = rootView.findViewById(R.id.product_purchase_box2);
         product_purchase_box3 = rootView.findViewById(R.id.product_purchase_box3);
@@ -179,27 +93,9 @@ public class Product_Purchase_Fragment extends Fragment {
         product_purchase_box6 = rootView.findViewById(R.id.product_purchase_box6);
         product_purchase_btn1 = rootView.findViewById(R.id.product_purchase_btn1);
 
-=======
->>>>>>> parent of 74edf15 (01/27 20:38 최성욱)
-
-        rotate = AnimationUtils.loadAnimation(context, R.anim.imge_rotate_deg180);
-        mainActivity = (MainActivity) getActivity();
-
-<<<<<<< HEAD
-=======
-        //========= 토글 기능 =============
-        toggleBtn(product_purchase_toggle1, rotate, product_purchase_updown1, isClick1);
-        toggleBtn(product_purchase_toggle2, rotate, product_purchase_updown2, isClick2);
-        toggleBtn(product_purchase_toggle3, rotate, product_purchase_updown3, isClick3);
-        toggleBtn(product_purchase_toggle4, rotate, product_purchase_updown4, isClick4);
-
-=======
->>>>>>> d785786d5ea155274c65bfa3305b907797f3797b
 
         mainActivity = (MainActivity) getActivity();
->>>>>>> 기근태
 
-<<<<<<< HEAD
         //========= 토글 기능 =============
 
         ArrayList<Boolean> clickList = new ArrayList<>();
@@ -216,9 +112,6 @@ public class Product_Purchase_Fragment extends Fragment {
         cardCheck(product_purchase_box1, product_purchase_updown5);
         cardCheck(product_purchase_box2, product_purchase_updown6);
 
-=======
->>>>>>> parent of 74edf15 (01/27 20:38 최성욱)
->>>>>>> d785786d5ea155274c65bfa3305b907797f3797b
         //========= 햄버커 기능 ==============
 
         DrawerLayout drawer = rootView.findViewById(R.id.drawer_layout);
@@ -283,7 +176,6 @@ public class Product_Purchase_Fragment extends Fragment {
             }
         });
 
-<<<<<<< HEAD
         product_purchase_btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -293,22 +185,11 @@ public class Product_Purchase_Fragment extends Fragment {
             }
         });
 
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> d785786d5ea155274c65bfa3305b907797f3797b
 
 
         setRec1();
 
 
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 기근태
->>>>>>> parent of 74edf15 (01/27 20:38 최성욱)
->>>>>>> d785786d5ea155274c65bfa3305b907797f3797b
         return rootView;
     }
 
@@ -316,12 +197,6 @@ public class Product_Purchase_Fragment extends Fragment {
     public void changeFragment(Fragment fragment){
         mainActivity.getSupportFragmentManager().beginTransaction().replace(R.id.product_purchase_container , fragment).commit();
     }
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> d785786d5ea155274c65bfa3305b907797f3797b
 
     public void setRec1(){
         manager = new LinearLayoutManager(context, RecyclerView.VERTICAL, false);
@@ -332,7 +207,6 @@ public class Product_Purchase_Fragment extends Fragment {
         product_purchase_rec1.setAdapter(adapter_rec1);
     }
 
-<<<<<<< HEAD
     public void toggleBtn(ImageView toggle, LinearLayout linear, ArrayList<Boolean> clickList, int postion){
         toggle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -345,24 +219,11 @@ public class Product_Purchase_Fragment extends Fragment {
                     toggle.setImageResource(R.drawable.up1);
                     linear.setVisibility(View.GONE);
                     clickList.set(postion, true);
-=======
-    public void toggleBtn(ImageView toggle, Animation rotate, LinearLayout linear, boolean isClick){
-        toggle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(isClick == true){
-                    toggle.startAnimation(rotate);
-                    linear.setVisibility(View.GONE);
-                } else {
-                    toggle.startAnimation(rotate);
-                    linear.setVisibility(View.VISIBLE);
->>>>>>> d785786d5ea155274c65bfa3305b907797f3797b
                 }
             }
         });
     }
 
-<<<<<<< HEAD
     public void cardCheck(CheckBox checkBox, LinearLayout linear){
         checkBox.setOnClickListener(new CheckBox.OnClickListener() {
             @Override
@@ -376,10 +237,4 @@ public class Product_Purchase_Fragment extends Fragment {
         }) ;
     }
 
-=======
-
-=======
->>>>>>> 기근태
->>>>>>> parent of 74edf15 (01/27 20:38 최성욱)
->>>>>>> d785786d5ea155274c65bfa3305b907797f3797b
 }
