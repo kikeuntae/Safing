8949eaf6d1272076556044a5437a70b499b98740
seongukchoi <cabin2351@gmail.com>
2022-02-04 09:37:43 +0900

    02/04 09:37 최성욱

---
+++ Safing Android/Safing/app/src/main/java/com/example/safing/mypage/fragment/JoinFragment.java
@@ -0,0 +1,32 @@
+package com.example.safing.mypage.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.safing.R;
+import com.example.safing.MainActivity;
+
+public class JoinFragment extends Fragment {
+    Context context;
+    LinearLayoutManager manager;
+    MainActivity mainActivity = new MainActivity();
+
+
+    public JoinFragment(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_create, container, false);
+
+        return rootView;
+    }
+}
