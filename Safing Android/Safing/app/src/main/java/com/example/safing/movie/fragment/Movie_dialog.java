9726bd8b14c47db195acaf18b700d175c634f884
명운 <48548596+WAWWW121@users.noreply.github.com>
2022-02-10 12:33:26 +0900

    TEST 커밋 다운 X

---
+++ Safing Android/Safing/app/src/main/java/com/example/safing/movie/fragment/Movie_dialog.java
@@ -0,0 +1,55 @@
+package com.example.safing.movie.fragment;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.safing.R;
+
+public class Movie_dialog extends AppCompatActivity {
+    private Context context;
+    private int position;
+
+    public Movie_dialog(Context context, int position) {
+        this.context = context;
+        this.position = position;
+    }
+
+    // 호출할 다이얼로그 함수를 정의한다.
+    public void callFunction() {
+
+        // 커스텀 다이얼로그를 정의하기위해 Dialog클래스를 생성한다.
+        final Dialog dlg = new Dialog(context);
+
+        // 액티비티의 타이틀바를 숨긴다.
+        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // 커스텀 다이얼로그의 레이아웃을 설정한다.
+        dlg.setContentView(R.layout.movie_comment_dialog);
+
+        // 커스텀 다이얼로그를 노출한다.
+        dlg.show();
+
+        // 커스텀 다이얼로그의 각 위젯들을 정의한다.
+
+        final ImageButton cancelButton = (ImageButton) dlg.findViewById(R.id.movie_comment_exit);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "취소 했습니다.", Toast.LENGTH_SHORT).show();
+
+                // 커스텀 다이얼로그를 종료한다.
+                dlg.dismiss();
+            }
+        });
+    }
+}
